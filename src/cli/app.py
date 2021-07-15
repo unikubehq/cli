1d4b633f90e4914b61976734824957f06f834ea7
Christian Busch <christian@blueshoe.de>
2021-07-15 15:55:33 +0200

    fix(app list): console input

--- src/cli/app.py
+++ src/cli/app.py
@@ -31,8 +31,8 @@ def get_deck_from_arguments(ctx, organization_id: str, project_id: str, deck_id:
             message_no_choices="No cluster is running.",
             choices=cluster_choices,
         )
-        if project_id is None:
-            console.exit_generic_error()
+        if project_selected is None:
+            exit(1)
 
         project_id = re.search(r"\((.*?)\)", project_selected).group(1)
     else:
