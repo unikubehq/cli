2bfe229bd4d3c00e4a77d0c0e189db98cc341a8a
Christian Busch <christian@blueshoe.de>
2021-07-19 11:33:46 +0200

    refactor: remove exit_error_with_message()

--- src/cli/console/__init__.py
+++ src/cli/console/__init__.py
@@ -1,4 +1,4 @@
-from .exit import exit_error_with_message, exit_generic_error, exit_login_required
+from .exit import exit_generic_error, exit_login_required
 from .input import list
 from .logger import debug, error, info, success, warning
 from .output import table
