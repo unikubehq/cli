213c7885ed7957b8d1edc3f1522960ca7dc2cca9
Christian Busch <christian@blueshoe.de>
2021-07-19 11:33:46 +0200

    refactor: remove exit_error_with_message()

--- src/cli/console/exit.py
+++ src/cli/console/exit.py
@@ -9,8 +9,3 @@ def exit_login_required():
 def exit_generic_error():
     error("Something went wrong!")
     exit(1)
-
-
-def exit_error_with_message(message: str):
-    error(message)
-    exit(1)
