164bc7f9b523413b94b5522a5f210c1763e6609d
Christian Busch <christian@blueshoe.de>
2021-08-26 14:47:56 +0200

    refactor(context): orga commands

--- src/cli/orga.py
+++ src/cli/orga.py
@@ -1,10 +1,10 @@
 import click
 
 import src.cli.console as console
+from src.context.helper import convert_context_arguments
 from src.graphql import GraphQL
 from src.helpers import select_entity
 from src.keycloak.permissions import KeycloakPermissions
-from src.storage.user import get_local_storage_user
 
 
 @click.command()
@@ -21,7 +21,6 @@ def list(ctx, **kwargs):
         keycloak_permissions = KeycloakPermissions(authentication=ctx.auth)
         permission_list = keycloak_permissions.get_permissions_by_scope("organization:*")
     except Exception as e:
-        permission_list = None
         console.debug(e)
         console.exit_generic_error()
 
@@ -56,48 +55,35 @@ def info(ctx, organization, **kwargs):
     Display further information of the selected organization.
     """
 
+    organization_id, _, _ = convert_context_arguments(ctx=ctx, organization_argument=organization)
+    context = ctx.context.get(organization=organization_id)
+    organization_id = context.organization_id
+
+    # argument
+    if not organization_id:
+        organization_id = console.organization_list(ctx)
+        if not organization_id:
+            return None
+
     # GraphQL
     try:
         graph_ql = GraphQL(authentication=ctx.auth)
         data = graph_ql.query(
             """
-            {
-                allOrganizations {
-                    results {
-                        id
-                        title
-                        description
-                    }
+            query($id: UUID!) {
+                organization(id: $id) {
+                    id
+                    title
+                    description
                 }
             }
-            """
+            """,
+            query_variables={"id": organization_id},
         )
+        organization_selected = data["organization"]
     except Exception as e:
-        data = None
         console.debug(e)
         console.exit_generic_error()
-
-    organization_list = data["allOrganizations"]["results"]
-
-    # argument
-    if not organization:
-        # argument from context
-        context = ctx.context.get()
-        if context.organization_id:
-            organization_instance = ctx.context.get_organization()
-            organization = organization_instance["title"] + f"({organization_instance['id']})"
-
-        # argument from console
-        else:
-            organization = console.list(
-                message="Please select an organization",
-                choices=[organization["title"] + f"({organization['id']})" for organization in organization_list],
-            )
-            if organization is None:
-                return None
-
-    # select
-    organization_selected = select_entity(organization_list, organization)
 
     # console
     if organization_selected:
