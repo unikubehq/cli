bc948add458ac1aa94094f3df55933504d41eb7d
Robert Stein <SteinRobert@users.noreply.github.com>
2021-11-30 21:40:22 +0100

    feature: add age to app list

--- src/cli/helper.py
+++ src/cli/helper.py
@@ -1,3 +1,4 @@
+import datetime
 import errno
 import os
 
@@ -9,3 +10,9 @@ def exist_or_create(folder):
         except OSError as exc:
             if exc.errno != errno.EEXIST:
                 raise
+
+
+def age_from_timestamp(timestamp):
+    return str(
+        datetime.timedelta(seconds=datetime.datetime.now(tz=datetime.timezone.utc).timestamp() - timestamp)
+    ).split(".")[0]
